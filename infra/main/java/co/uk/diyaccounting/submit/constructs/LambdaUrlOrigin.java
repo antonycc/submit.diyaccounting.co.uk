f6ec931b65bf0d932a7b99a8813b8f8985d4dcbd
Antony at Polycode <antony@polycode.co.uk>
2025-10-26 23:41:44 +0100

    chore: comment out unused Lambda function URLs in SubmitDelivery

--- infra/main/java/co/uk/diyaccounting/submit/constructs/LambdaUrlOrigin.java
+++ infra/main/java/co/uk/diyaccounting/submit/constructs/LambdaUrlOrigin.java
@@ -1,8 +1,10 @@
 package co.uk.diyaccounting.submit.constructs;
 
-import static co.uk.diyaccounting.submit.utils.Kind.infof;
-
-import java.util.List;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.cloudwatch.Alarm;
+import software.amazon.awscdk.services.cloudwatch.ComparisonOperator;
+import software.amazon.awscdk.services.cloudwatch.MetricOptions;
+import software.amazon.awscdk.services.cloudwatch.TreatMissingData;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
 import software.amazon.awscdk.services.ecr.RepositoryAttributes;
@@ -14,6 +16,10 @@ import software.amazon.awscdk.services.lambda.Tracing;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awscdk.services.logs.LogGroupProps;
 import software.constructs.Construct;
+
+import java.util.List;
+
+import static co.uk.diyaccounting.submit.utils.Kind.infof;
 
 public class LambdaUrlOrigin {
 
@@ -62,5 +68,49 @@ public class LambdaUrlOrigin {
                         .removalPolicy(props.logGroupRemovalPolicy())
                         .build());
         infof("Created log group %s with retention %s", this.logGroup.getNode().getId(), props.logGroupRetention());
+
+        // Alarms: a small set of useful, actionable Lambda alarms
+        // 1) Errors >= 1 in a 5-minute period
+        Alarm.Builder.create(scope, props.idPrefix() + "-ErrorsAlarm")
+                .alarmName(props.functionName() + "-errors")
+                .metric(this.lambda.metricErrors().with(MetricOptions.builder()
+                        .period(Duration.minutes(5))
+                        .build()))
+                .threshold(1)
+                .evaluationPeriods(1)
+                .comparisonOperator(ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD)
+                .treatMissingData(TreatMissingData.NOT_BREACHING)
+                .alarmDescription("Lambda errors >= 1 for function " + this.lambda.getFunctionName())
+                .build();
+
+        // 2) Throttles >= 1 in a 5-minute period
+        Alarm.Builder.create(scope, props.idPrefix() + "-ThrottlesAlarm")
+                .alarmName(props.functionName() + "-throttles")
+                .metric(this.lambda.metricThrottles().with(MetricOptions.builder()
+                        .period(Duration.minutes(5))
+                        .build()))
+                .threshold(1)
+                .evaluationPeriods(1)
+                .comparisonOperator(ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD)
+                .treatMissingData(TreatMissingData.NOT_BREACHING)
+                .alarmDescription("Lambda throttles >= 1 for function " + this.lambda.getFunctionName())
+                .build();
+
+        // 3) High duration (p95) approaching timeout (>= 80% of configured timeout)
+        // Lambda Duration metric unit is milliseconds. Convert timeout to ms and apply 80% threshold.
+        double timeoutMs = props.timeout().toSeconds().doubleValue() * 1000.0;
+        double highDurationThresholdMs = timeoutMs * 0.8;
+        Alarm.Builder.create(scope, props.idPrefix() + "-HighDurationP95Alarm")
+                .alarmName(props.functionName() + "-high-duration-p95")
+                .metric(this.lambda.metricDuration().with(MetricOptions.builder()
+                        .statistic("p95")
+                        .period(Duration.minutes(5))
+                        .build()))
+                .threshold(highDurationThresholdMs)
+                .evaluationPeriods(1)
+                .comparisonOperator(ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD)
+                .treatMissingData(TreatMissingData.NOT_BREACHING)
+                .alarmDescription("Lambda p95 duration >= 80% of timeout for function " + this.lambda.getFunctionName())
+                .build();
     }
 }
