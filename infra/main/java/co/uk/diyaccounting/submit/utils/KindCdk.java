0f6abb4a26578aa583bc283823d3e2d120046fd9
Antony at Polycode <antony@polycode.co.uk>
2026-01-18 21:05:33 +0000

    feat: update TEST_AUTH_PROVIDER to use cognito-native for CI environment

--- infra/main/java/co/uk/diyaccounting/submit/utils/KindCdk.java
+++ infra/main/java/co/uk/diyaccounting/submit/utils/KindCdk.java
@@ -8,9 +8,19 @@ package co.uk.diyaccounting.submit.utils;
 import static co.uk.diyaccounting.submit.utils.Kind.infof;
 import static co.uk.diyaccounting.submit.utils.Kind.warnf;
 
+import java.util.List;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.customresources.AwsCustomResource;
+import software.amazon.awscdk.customresources.AwsCustomResourcePolicy;
+import software.amazon.awscdk.customresources.AwsSdkCall;
+import software.amazon.awscdk.customresources.PhysicalResourceId;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+import software.amazon.awscdk.services.logs.ILogGroup;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awssdk.utils.StringUtils;
 import software.constructs.Construct;
 
@@ -56,5 +66,37 @@ public class KindCdk {
                     "CDK_DEFAULT_ACCOUNT or CDK_DEFAULT_REGION environment variables are not set, using environment agnostic stacks");
         }
         return primaryEnv;
+    }
+
+    /**
+     * Creates a LogGroup idempotently using AwsCustomResource.
+     * Uses createLogGroup API with ignoreErrorCodesMatching("ResourceAlreadyExistsException")
+     * so deployments succeed whether the log group exists or not.
+     *
+     * @param stack The stack to create the log group in
+     * @param id The construct ID prefix
+     * @param logGroupName The name of the log group
+     * @return ILogGroup reference to the log group
+     */
+    public static ILogGroup ensureLogGroup(Stack stack, String id, String logGroupName) {
+        AwsSdkCall createLogGroupCall = AwsSdkCall.builder()
+                .service("CloudWatchLogs")
+                .action("createLogGroup")
+                .parameters(Map.of("logGroupName", logGroupName))
+                .physicalResourceId(PhysicalResourceId.of(logGroupName))
+                .ignoreErrorCodesMatching("ResourceAlreadyExistsException")
+                .build();
+
+        AwsCustomResource.Builder.create(stack, id + "-EnsureLogGroup")
+                .onCreate(createLogGroupCall)
+                .onUpdate(createLogGroupCall)
+                .policy(AwsCustomResourcePolicy.fromStatements(List.of(PolicyStatement.Builder.create()
+                        .actions(List.of("logs:CreateLogGroup"))
+                        .resources(List.of("arn:aws:logs:" + stack.getRegion() + ":" + stack.getAccount()
+                                + ":log-group:" + logGroupName + ":*"))
+                        .build())))
+                .build();
+
+        return LogGroup.fromLogGroupName(stack, id + "-LogGroup", logGroupName);
     }
 }
